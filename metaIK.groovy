52e8c199de28025a43aa09806b41edca2817dbdb
Xavier <Xavier@130.215.172.82>
2019-02-04 16:46:50 -0500

    Deleted comments

--- metaIK.groovy
+++ metaIK.groovy
@@ -127,55 +127,7 @@ public class scriptJavaIKModel implements DhInverseSolver {
             System.out.println(inv[2]);
             System.out.println(inv[3]);
             
-            
-//            double r1 = Math.sqrt(Math.pow(x, 2) + Math.pow(y,2)+Math.pow(z, 2));
-//            System.out.println("Limb total vector length = "+r1);
-//            double footTiltAngle = Math.toDegrees(Math.atan2(z, y));
-//            System.out.println("Tilting the foot angle " +footTiltAngle );
-//            Transform wristcenter= new Transform()
-//            						.movex(-l4_d)
-//            						.roty(footTiltAngle+ang) // this should the  the foot angle
-//            						.rotz(-theta1Degrees)
-//            						.movex(x)
-//            						.movey(y)
-//            						.movez(z);
-//            
-//            
-//            double wristCenterX = wristcenter.getX();
-//            double wristCenterY = wristcenter.getY();
-//            double wristCenterZ = wristcenter.getZ();
-//            
-//            System.out.println("Wrist center = x "+wristCenterX+" y "+wristCenterY+" z "+wristCenterZ);
-//            
-//            double r2 = Math.sqrt(Math.pow(wristCenterX, 2) + Math.pow(wristCenterY,2)+Math.pow(wristCenterZ, 2)); 
-//            System.out.println("Wrist center r2 =" +r2);
-//            
-//            double a = Math.asin(wristCenterX / r2);
-//            double Px = wristCenterX;
-//            double Py = Math.cos(a)*r2;  
-//            System.out.println("a =" +a);
-//            System.out.println("Px =" +Px);
-//            System.out.println("Py =" +Py);
-            		
-            
-//            double x1 = r1;
-//            double y1 = z;
-//            double Px = x1 - l4_d * Math.sin(ang);
-//            double Py = y1 - l4_d * Math.cos(ang);
-//            // Make below negative to switch to other angle
-//            double theta3_1 = -1* (Math.acos(((Math.pow(Px, 2) + Math.pow(Py,2)) - (Math.pow(l2_d,2) + Math.pow(l3_d, 2))) / (2 * l2_d * l3_d)));
-//            double theta3_2 = theta3_1 * -1;
-//
-//            double B = Math.atan2(Py , Px);
-//            double Y = Math.acos((Math.pow(Px, 2) + Math.pow(Py, 2) + Math.pow(l2_d, 2) - Math.pow(l3_d, 2)) / (2 * l2_d * Math.sqrt(Math.pow(Px, 2) + Math.pow(Py, 2))));
-//
-//            double theta2_1 = B + Y;
-//            double theta2_2 = B - Y;
-//
-//            double theta4_1 = (Math.PI) - (theta2_1 + theta3_1);
-//            double theta4_2 = (Math.PI) - (theta2_2 + theta3_2);
-		
-            //System.out.println("\r\n\r\nJoint Vector = " + inv + "\r\n\r\n");
+
             if(Double.isNaN(inv[0]) || Double.isNaN(inv[1]) || Double.isNaN(inv[2]) || Double.isNaN(inv[3]))
             	throw new ArithmeticException("Can't move to that position");
             else
