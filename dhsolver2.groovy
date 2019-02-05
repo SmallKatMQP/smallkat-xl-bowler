e3e8c6dae6a1b96b2cc311054b137e482c85bc9e
xaveagle <xaveagle@gmail.com>
2019-02-04 22:24:33 -0500

    Create dhsolver2.groovy

---
+++ dhsolver2.groovy
@@ -0,0 +1,6 @@
+//Debugger IK
+//return new com.neuronrobotics.smallkat.JavaIKModel();
+
+ScriptingEngine.gitScriptRun("https://github.com/SmallKatMQP/smallkat-xl-bowler.git", "metaIK.groovy", 
+                                     
+                                     [1]);
