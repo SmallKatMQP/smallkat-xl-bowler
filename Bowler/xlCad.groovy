a8e3d40c2a0e2a9f10ba98a4d3a0fed1299cefbf
Alex Tacescu <alextac98@gmail.com>
2019-02-17 20:36:38 -0500

    Merge simpleCad and xlCAD to allow for user to select model by changing a boolean

--- Bowler/xlCad.groovy
+++ Bowler/xlCad.groovy
@@ -11,6 +11,10 @@ println "Loading STL file"
 // Load an STL file from a git repo
 // Loading a local file also works here
 String giturl = "https://github.com/SmallKatMQP/smallkat-xl-bowler.git";
+
+// TRUE to render the model, FALSE to render a stick figure
+boolean isFullModel = true;
+
 return new ICadGenerator(){
 
 	private CSG moveDHValues(CSG incoming,DHLink dh ){
@@ -22,6 +26,36 @@ return new ICadGenerator(){
 
 	@Override
 	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
+
+		if (isFullModel){
+			return generateFullModel(d, linkIndex);
+		} else {
+			return generateStickModel(d, linkIndex);
+		}
+
+	}
+
+@Override
+	public ArrayList<CSG> generateBody(MobileBase b ) {
+		ArrayList<CSG> allCad=new ArrayList<>();
+		File mainBodyFile = ScriptingEngine.fileFromGit("https://github.com/SmallKatMQP/smallkat-xl-bowler.git", "cad/Body.stl");
+		
+	// Load the .CSG from the disk and cache it in memory
+		CSG body = new Cube(10).toCSG();
+		if (isFullModel){
+			body  = Vitamins.get(mainBodyFile);
+		}
+		
+		body.setManipulator(b.getRootListener());
+		body.setColor(javafx.scene.paint.Color.WHITE)
+		def parts = [body ] as ArrayList<CSG>
+		for(int i=0;i<parts.size();i++){
+			parts.get(i).setColor(javafx.scene.paint.Color.GRAY)
+		}
+		return parts;
+	}
+
+	private ArrayList<CSG> generateFullModel(DHParameterKinematics d, int linkIndex){
 		ArrayList<CSG> allCad=new ArrayList<>();
 		String limbName = d.getScriptingName()
 		File legFile = null
@@ -42,9 +76,9 @@ return new ICadGenerator(){
 
 		if(limbName.contentEquals("Tail")){
 		  if(linkIndex ==0){
-		    // legFile = ScriptingEngine.fileFromGit(giturl, "cad/TailJoint.stl");
+		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/TailJoint.stl");
 		  } else if(linkIndex ==1){
-		    // legFile = ScriptingEngine.fileFromGit(giturl, "cad/Tail.stl");
+		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/Tail.stl");
 		  } else if (linkIndex == 2) {
 		    reteurn allCad;
 		  } else {
@@ -162,25 +196,132 @@ return new ICadGenerator(){
 			parts.get(i).setColor(javafx.scene.paint.Color.RED)
 		}
 		return parts;
-
 	}
-
-
-
-@Override
-	public ArrayList<CSG> generateBody(MobileBase b ) {
+	
+	private ArrayList<CSG> generateStickModel(DHParameterKinematics d, int linkIndex){
 		ArrayList<CSG> allCad=new ArrayList<>();
-		File mainBodyFile = ScriptingEngine.fileFromGit("https://github.com/SmallKatMQP/smallkat-xl-bowler.git", "cad/Body.stl");
-
-	// Load the .CSG from the disk and cache it in memory
-		CSG body  = Vitamins.get(mainBodyFile);
-
-		body.setManipulator(b.getRootListener());
-		body.setColor(javafx.scene.paint.Color.WHITE)
-		def parts = [body ] as ArrayList<CSG>
-		for(int i=0;i<parts.size();i++){
-			parts.get(i).setColor(javafx.scene.paint.Color.GRAY)
+		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
+		DHLink dh = dhLinks.get(linkIndex)
+		// Hardware to engineering units configuration
+		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
+		// Engineering units to kinematics link (limits and hardware type abstraction)
+		AbstractLink abstractLink = d.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
+		// Transform used by the UI to render the location of the object
+		Affine manipulator = dh.getListener();
+		Affine lastLinkFrame
+		
+		if(linkIndex==0)
+			lastLinkFrame=d.getRootListener()
+		else
+			lastLinkFrame=dhLinks.get(linkIndex-1).getListener();
+		
+		def rVal = new Cube(dh.getR()>0?dh.getR():5,5,5).toCSG()
+					.toXMax()
+					.toZMax()
+		rVal.setColor(javafx.scene.paint.Color.RED)
+		CSG profile = new Cube(1,// x dimention
+					20,// y dimention
+				
+				1//  Z dimention
+				)
+				.toCSG()// converts it to a CSG tor display
+				.toYMin()
+				.toZMin()
+				//.toXMin()
+				
+		CSG theta;
+		double thetaval = Math.toDegrees(dh.getTheta())
+		if(Math.abs(thetaval)>10){
+			theta= CSG.unionAll(
+			Extrude.revolve(profile,
+						0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+						Math.abs(thetaval),// degrees through wich it should sweep
+						(int)10)//number of sweep increments
+			)
+		}else{
+			theta = profile
 		}
+		
+		if(thetaval>0){
+			theta= theta.rotz(-thetaval)
+		}
+		theta= theta.rotz(90)
+		.movez(-0.5)
+		theta.setColor(thetaval>0?javafx.scene.paint.Color.BLUE:javafx.scene.paint.Color.AQUA)
+
+		CSG alpha;
+		double alphaVal = Math.toDegrees(dh.getAlpha())
+		if(Math.abs(alphaVal)>10){
+			alpha= CSG.unionAll(
+			Extrude.revolve(profile,
+						0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+						Math.abs(alphaVal),// degrees through wich it should sweep
+						(int)10)//number of sweep increments
+			)
+			//.rotz(alphaVal<0?-alphaVal:0)
+		}else{
+			alpha = profile
+		}
+		alpha= alpha.roty(90)
+		if(alphaVal>0){
+			alpha= alpha.rotx(alphaVal)	
+		}
+		alpha= alpha
+			.rotx(-90)
+			.movex(-dh.getR())
+		alpha.setColor(alphaVal>0?javafx.scene.paint.Color.YELLOW:javafx.scene.paint.Color.GOLDENROD)
+
+		def dpart = new Cube(5,5,dh.getD()>0?dh.getD():2.5).toCSG()
+					.toZMin()
+		double upperLimit = -abstractLink.getMaxEngineeringUnits()
+		double lowerLimit = -abstractLink.getMinEngineeringUnits()
+		double totalRange = -(upperLimit-lowerLimit)
+		def min = 20
+		if(totalRange>360-min)
+			totalRange=360-min
+		if(totalRange<min)
+			totalRange=min
+		println "Link range = "+totalRange
+		def rangeComp = 360-totalRange
+		def orentationAdjust = -thetaval+90
+		def Range
+		if(rangeComp>min)
+			Range = CSG.unionAll(
+			Extrude.revolve(profile,
+					0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+					rangeComp,// degrees through wich it should sweep
+					(int)min)//number of sweep increments
+			)
+		else
+			Range =profile
+
+		Range=Range
+			.rotz(lowerLimit+orentationAdjust)
+			.movez(-2)
+
+
+		Range.setColor(javafx.scene.paint.Color.LIGHTGREEN)
+		
+		def upperLim = profile
+					.rotz(upperLimit+orentationAdjust)
+					.setColor(javafx.scene.paint.Color.HOTPINK)
+		def lowerLim = profile
+					.rotz(lowerLimit+orentationAdjust)
+					.setColor(javafx.scene.paint.Color.WHITE)
+		def zeroLim = profile
+					.rotz(orentationAdjust)
+					.setColor(javafx.scene.paint.Color.INDIGO)
+		def lastFrameParts = [theta,dpart,upperLim,lowerLim,zeroLim,Range]
+		def parts = [rVal,alpha] as ArrayList<CSG>
+		for(int i=0;i<parts.size();i++){
+			parts.get(i).setManipulator(manipulator);
+			//parts.get(i).setColor(javafx.scene.paint.Color.RED)
+		}
+		for(int i=0;i<lastFrameParts.size();i++){
+			lastFrameParts.get(i).setManipulator(lastLinkFrame);
+			//parts.get(i).setColor(javafx.scene.paint.Color.RED)
+		}
+		parts.addAll(lastFrameParts)
 		return parts;
 	}
 }
