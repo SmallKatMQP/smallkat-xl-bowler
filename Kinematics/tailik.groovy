5096f733736adb49be9ebdca71cb3eff539e9593
Alex Tacescu <alextac98@gmail.com>
2019-02-17 20:57:56 -0500

    Add base IK algorithms for head and tail

---
+++ Kinematics/tailik.groovy
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+import com.neuronrobotics.sdk.addons.kinematics.DHChain;
+import com.neuronrobotics.sdk.addons.kinematics.DHLink;
+import com.neuronrobotics.sdk.addons.kinematics.DhInverseSolver;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import com.neuronrobotics.sdk.common.Log;
+import Jama.Matrix;
+
+return new DhInverseSolver() {
+
+	@Override
+	public double[] inverseKinematics(TransformNR target,
+			double[] jointSpaceVector,DHChain chain ) {
+
+    int linkNum = jointSpaceVector.length;
+    double [] inv = new double[linkNum];
+
+    inv[0] = 0;
+    inv[1] = 0;
+
+		return inv;
+	}
+};
