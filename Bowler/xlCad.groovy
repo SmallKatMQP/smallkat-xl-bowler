3470e4ac56b388bccfc7c02cc14b160933ba84a7
Alex Tacescu <alextac98@gmail.com>
2019-02-24 20:50:21 -0500

    Refactor xlCad to combine the dh Cad and the full cad

--- Bowler/xlCad.groovy
+++ Bowler/xlCad.groovy
@@ -24,23 +24,12 @@ return new ICadGenerator(){
 
 	}
 
-	@Override
-	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
-
-		if (isFullModel){
-			return generateFullModel(d, linkIndex);
-		} else {
-			return generateStickModel(d, linkIndex);
-		}
-
-	}
-
 @Override
 	public ArrayList<CSG> generateBody(MobileBase b ) {
 		ArrayList<CSG> allCad=new ArrayList<>();
 		File mainBodyFile = ScriptingEngine.fileFromGit("https://github.com/SmallKatMQP/smallkat-xl-bowler.git", "cad/Body.stl");
 
-	// Load the .CSG from the disk and cache it in memory
+		// Load the .CSG from the disk and cache it in memory
 		CSG body = new Cube(10).toCSG();
 		if (isFullModel){
 			body  = Vitamins.get(mainBodyFile);
@@ -55,26 +44,23 @@ return new ICadGenerator(){
 		return parts;
 	}
 
-	private ArrayList<CSG> generateFullModel(DHParameterKinematics d, int linkIndex){
+@Override
+	public ArrayList<CSG> generateCad(DHParameterKinematics dhParams, int linkIndex){
 		ArrayList<CSG> allCad=new ArrayList<>();
-		String limbName = d.getScriptingName()
+		String limbName = dhParams.getScriptingName()
 		File legFile = null
-		// boolean mirror=true
-		// if(limbName.contentEquals("DefaultLeg3")||limbName.contentEquals("DefaultLeg4")){
-		// 	println "Mirror leg parts"
-		// 	mirror=false
-		// }
-		TransformNR  legRoot= d.getRobotToFiducialTransform()
-		def leftSide=false
-		def rear = true
-		if(legRoot.getY()>0){
-			leftSide=true;
-		}
-		if(legRoot.getX()>0){
-			rear=false;
-		}
 
-		if(limbName.contentEquals("Tail")){
+		// TransformNR  legRoot= dhParams.getRobotToFiducialTransform()
+		// def leftSide=false
+		// def rear = true
+		// if(legRoot.getY()>0){
+		// 	leftSide=true;
+		// }
+		// if(legRoot.getX()>0){
+		// 	rear=false;
+		// }
+
+		if(limbName.contentEquals("Tail") && isFullModel){
 		  if(linkIndex ==0){
 		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/TailJoint.stl");
 		  } else if(linkIndex ==1){
@@ -84,7 +70,7 @@ return new ICadGenerator(){
 		  } else {
 		    println "Incorrect Tail Index"
 		  }
-		} else if(limbName.contentEquals("Head")){
+		} else if(limbName.contentEquals("Head") && isFullModel){
 		  if(linkIndex >2)
 		    return allCad;
 		  if(linkIndex ==0){
@@ -100,7 +86,7 @@ return new ICadGenerator(){
 		    print "Incorrect Head Index: "
 		    println linkIndex
 		  }
-		} else if (limbName.contentEquals("FrontLeft")){
+		} else if (limbName.contentEquals("FrontLeft") && isFullModel){
 			println "FrontLeft"
 		  if(linkIndex == 0){
 		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/Shoulder_FL2.stl");
@@ -114,7 +100,7 @@ return new ICadGenerator(){
 		    print "Incorrect FrontLeft Leg Index: "
 		    println linkIndex
 		  }
-		} else if (limbName.contentEquals("FrontRight")){
+		} else if (limbName.contentEquals("FrontRight") && isFullModel){
 			println "FrontRight"
 		  if(linkIndex == 0){
 		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/Shoulder_FR2.stl");
@@ -128,7 +114,7 @@ return new ICadGenerator(){
 		    print "Incorrect FrontRight Leg Index: "
 		    println linkIndex
 		  }
-		} else if (limbName.contentEquals("BackLeft")){
+		} else if (limbName.contentEquals("BackLeft") && isFullModel){
 			println "BackLeft"
 		  if(linkIndex == 0){
 		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/Shoulder_BL2.stl");
@@ -142,7 +128,7 @@ return new ICadGenerator(){
 		    print "Incorrect BackLeft Leg Index: "
 		    println linkIndex
 		  }
-		} else if (limbName.contentEquals("BackRight")){
+		} else if (limbName.contentEquals("BackRight") && isFullModel){
 			println "BackRight"
 		  if(linkIndex == 0){
 		    legFile = ScriptingEngine.fileFromGit(giturl, "cad/Shoulder_BR2.stl");
@@ -161,34 +147,34 @@ return new ICadGenerator(){
 		  println limbName
 		}
 
-		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
-		DHLink dh = dhLinks.get(linkIndex)
-		// Hardware to engineering units configuration
-		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
-		// Engineering units to kinematics link (limits and hardware type abstraction)
-		AbstractLink abstractLink = d.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
-		// Transform used by the UI to render the location of the object
-		Affine manipulator = dh.getListener();
-
 		// Load the .CSG from the disk and cache it in memory
 		println "Loading " +legFile
-		CSG body // = Vitamins.get(legFile)
+		
 		if (legFile == null){
-			body = new Cube(5).toCSG()
+			// Return the DH Parameter Viewer
+			return generateStickModel(dhParams, linkIndex)
 		} else {
-			body = Vitamins.get(legFile)
-		}
+			// Return the regular viewer
+			ArrayList<DHLink> dhLinks = dhParams.getChain().getLinks()
+			DHLink dh = dhLinks.get(linkIndex)
+			// Hardware to engineering units configuration
+			LinkConfiguration conf = dhParams.getLinkConfiguration(linkIndex);
+			// Engineering units to kinematics link (limits and hardware type abstraction)
+			AbstractLink abstractLink = dhParams.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
+			// Transform used by the UI to render the location of the object
+			Affine manipulator = dh.getListener();
 
-		body.setManipulator(manipulator);
+			CSG body = Vitamins.get(legFile).setManipulator(manipulator);
 
-		def parts = [body ] as ArrayList<CSG>
-		for(int i=0;i<parts.size();i++){
-			parts.get(i).setColor(javafx.scene.paint.Color.RED)
+			def parts = [body ] as ArrayList<CSG>
+			for(int i=0;i<parts.size();i++){
+				parts.get(i).setColor(javafx.scene.paint.Color.RED)
+			}
+			return parts
 		}
-		return parts;
 	}
 
-	private ArrayList<CSG> generateStickModel(DHParameterKinematics d, int linkIndex){
+	private ArrayList<CSG> generateStickModel(DHParameterKinematics dhParams, int linkIndex){
 		ArrayList<CSG> allCad=new ArrayList<>();
 		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
 		DHLink dh = dhLinks.get(linkIndex)
@@ -199,26 +185,24 @@ return new ICadGenerator(){
 		// Transform used by the UI to render the location of the object
 		Affine manipulator = dh.getListener();
 		Affine lastLinkFrame
-
+		
 		if(linkIndex==0)
 			lastLinkFrame=d.getRootListener()
 		else
 			lastLinkFrame=dhLinks.get(linkIndex-1).getListener();
-
-		def rVal = new Cube(dh.getR()>0?dh.getR():5,5,5).toCSG()
+		
+		def rVal = new Cube(dh.getR()>0?dh.getR():5,1,1).toCSG()
 					.toXMax()
 					.toZMax()
 		rVal.setColor(javafx.scene.paint.Color.RED)
-		CSG profile = new Cube(1,// x dimention
-					20,// y dimention
-
-				1//  Z dimention
-				)
+		CSG profile = new Cube(	1,		// x dimention
+													 20,		// y dimention
+														1)		// z dimention
 				.toCSG()// converts it to a CSG tor display
 				.toYMin()
 				.toZMin()
 				//.toXMin()
-
+				
 		CSG theta;
 		double thetaval = Math.toDegrees(dh.getTheta())
 		if(Math.abs(thetaval)>10){
@@ -231,7 +215,7 @@ return new ICadGenerator(){
 		}else{
 			theta = profile
 		}
-
+		
 		if(thetaval>0){
 			theta= theta.rotz(-thetaval)
 		}
@@ -254,24 +238,28 @@ return new ICadGenerator(){
 		}
 		alpha= alpha.roty(90)
 		if(alphaVal>0){
-			alpha= alpha.rotx(alphaVal)
+			alpha= alpha.rotx(alphaVal)	
 		}
 		alpha= alpha
 			.rotx(-90)
 			.movex(-dh.getR())
 		alpha.setColor(alphaVal>0?javafx.scene.paint.Color.YELLOW:javafx.scene.paint.Color.GOLDENROD)
 
-		def dpart = new Cube(5,5,dh.getD()>0?dh.getD():2.5).toCSG()
+		def dpart = new Cube(1,1,dh.getD()>0?dh.getD():1).toCSG()
 					.toZMin()
 		double upperLimit = -abstractLink.getMaxEngineeringUnits()
 		double lowerLimit = -abstractLink.getMinEngineeringUnits()
 		double totalRange = -(upperLimit-lowerLimit)
-		def min = 20
+		def min = 10
 		if(totalRange>360-min)
 			totalRange=360-min
 		if(totalRange<min)
 			totalRange=min
-		println "Link range = "+totalRange
+		def name = d.getScriptingName()
+		//println name
+		
+		def printit = name.equals("FrontRight")&&linkIndex==0
+		if(printit)println "\n\n\nLink range = "+totalRange+" "+upperLimit+" " +lowerLimit
 		def rangeComp = 360-totalRange
 		def orentationAdjust = -thetaval+90
 		def Range
@@ -284,14 +272,13 @@ return new ICadGenerator(){
 			)
 		else
 			Range =profile
-
+		
 		Range=Range
 			.rotz(lowerLimit+orentationAdjust)
 			.movez(-2)
 
-
+		
 		Range.setColor(javafx.scene.paint.Color.LIGHTGREEN)
-
 		def upperLim = profile
 					.rotz(upperLimit+orentationAdjust)
 					.setColor(javafx.scene.paint.Color.HOTPINK)
