7f4a8c1a2140926b04b8bacdae11f7d66470eaf4
keionbis <keion96@gmail.com>
2019-04-15 22:15:30 -0400

    calib

--- Bowler/DynamicWalking.groovy
+++ Bowler/DynamicWalking.groovy
@@ -20,7 +20,7 @@ enum WalkingState {
 }
 
 if(args==null){
-	double stepOverHeight = 50;
+	double stepOverHeight = 30;
 	long stepOverTime=500;// Servo loop times number of points times Nyquest doubeling
 	Double zLock=-205;
 	Closure calcHome = { DHParameterKinematics leg ->
@@ -35,10 +35,10 @@ if(args==null){
 				tr.translateY(-25)
 			}
 			if(legRoot.getX()>0){
-				tr.translateX(15)
+				tr.translateX(10)
 			}
 			if(legRoot.getX()<0){
-				tr.translateX(-15)
+				tr.translateX(-45)
 			}
 
 
@@ -847,7 +847,7 @@ return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
 	}
 	public void DriveArcLocal(MobileBase s, TransformNR n, double sec, boolean retry) {
 		
-		n=n.times(new TransformNR(0,0,0,new RotationNR(0,0,0)))
+		n=n.times(new TransformNR(0,0,0,new RotationNR(0,0,2)))
 		try{
 
 			//println "Walk update "+n
